af4564183b40b7f686bef64bc4cfaed47ecb5814
Azure <b00705046@ntu.edu.tw>
2014-05-03 01:25:51 +0800

    1:25

--- app.js
+++ app.js
@@ -5,28 +5,53 @@
  * @version 2014/04/26
  */
 
+var data = "";
+
 var http = require("http");
-var port = 1337;
 var request = require("request");
-var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
+var fs = require("fs"), filename = "main.html", encode = "utf8";
+var jsFile = "main.js";
+
+  fs.readFile(filename, encode, function(err, file) {
+  data += file;
+  data += '<div id="_container" class="box" style="height:800px; width:80%; overflow:hidden; margin:0 auto;">';
+  }); 
+
+
+var port = 1337;
+var url = "http://graph.facebook.com/aDozenOnions/photos?type=uploaded";
 
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
+ 
+  getPhoto(url, res);
+
   
-  var data = "<html><head></head><body>"
+}).listen(port);
+
+console.log("start server port: " + port);
+
+function getPhoto(url, res) {
+
   request.get(url, function (err, body, response) {
 
     response = JSON.parse(response);
     response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'>";
+      if( val.images[5].width < val.images[5].height){
+        data += "<img  class='item H' src='" + val.images[5].source + "' />";
+      }
+      else if( val.images[5].width > val.images[5].height){
+        data += "<img class='item W' src='" + val.images[5].source + "' />";
+      }
+      
+      
     });
     
-    data += "</body></html>";
+    data += "</div></body></html>";
     res.end(data);
   });
 
-}).listen(port);
-
-console.log("start server port: " + port);
+  
+}
 
 

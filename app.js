bc0df14c6cc1df46eb9a82b8ff71cb6cb01e9cfd
lulalachen <lulalachen@lulalachens-MacBook-Pro.local>
2014-05-03 20:09:07 +0800

    heheh

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var request = require("request");
-var port = 5000;
+var port = Number(process.env.PORT || 3000); // to deploy on Heroku!!!!!!!~~~~~~~~
 
 var url = "https://graph.facebook.com/2014.hsnu.at.ntu/photos?type=uploaded";
 //JSON is a global module not need to require//

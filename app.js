0c6506efc7a000ae53ce1543a9689f0114388c1b
caesar_chi <caesar_chi@hiiir.com>
2014-05-03 15:25:59 +0800

    update for view render

--- app.js
+++ app.js
@@ -8,22 +8,35 @@
 var http = require("http");
 var port = 1337;
 var request = require("request");
+var fs = require("fs");
 var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
+var template  = "";
+
+
+fs.readFile("./index.html", "utf8", function (err, result) {
+    template = result;
+});
+
+// also equal
+// template = fs.readFileSync("./index.html", "utf8");
 
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
-  // res.setHeader("Content-Type", "text/html");
-  var data = "<html><head></head><body>"
   request.get(url, function (err, body, response) {
+
+    // init data
+    var data = "";
 
     response = JSON.parse(response);
     response.data.forEach(function (val, idx) {
       data += "<img src='" + val.images[2].source + "'>";
     });
-    
-    data += "</body></html>";
+
+    // replace data
+    data = template.replace("{{content}}", data);
     res.end(data);
   });
+
 
 }).listen(port);
 

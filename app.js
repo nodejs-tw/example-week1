ce43feb403491cb5d45226d109f97a1ac305a529
lulalachen <lulalachen@lulalachens-MacBook-Pro.local>
2014-05-03 20:08:05 +0800

    heheh

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var http = require("http");
 var request = require("request");
-var port = 1337;
+var port = 5000;
 
 var url = "https://graph.facebook.com/2014.hsnu.at.ntu/photos?type=uploaded";
 //JSON is a global module not need to require//
 
 
-/*http.createServer (function(req, res) {
+http.createServer (function(req, res) {
 
   var datas = "<html><head></head><body>";
   
@@ -17,16 +17,13 @@ var url = "https://graph.facebook.com/2014.hsnu.at.ntu/photos?type=uploaded";
     result.data.forEach(function (val,idx) {
       if (val.comments != null){
         datas += "<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">"
-        datas += "<table> <thead><th colspan=\"3\">Facebook Messages</th></thead> <tbody>" + "<tr><td>";
-        datas += "<img src ='" + val.images[5].source + "'><br></td></tr>";
-        datas += "<tr><td>"
+        datas += "<img src ='" + val.images[5].source + "'><br>";
         val.comments.data.forEach(function (tt,idx){
         
-          datas += "<h3 style=\"font-color: 'blue' \">" + tt.from.name + "</h3></td>";
+          datas += "<h3 style=\"color: blue \">" + tt.from.name + "</h3>";
           datas += "<p>  " + tt.message + "</p>";
           //console.log(datas);
         });
-        datas += "</td></tr>"
       }
 
     });
@@ -36,11 +33,6 @@ var url = "https://graph.facebook.com/2014.hsnu.at.ntu/photos?type=uploaded";
   datas += "</body></html>";
   console.log("run");
 }).listen(port);
-*/
-//console.log("Server Created\nPort Number : "+port);
-http.createServer(function(req,res){
-	var datas += "<html><body>hello~~~~</body></html>";
-	res.end(datas);
-}).listen(port);
 
-console.log("Server Created \n");
+console.log("Server Created\nPort Number : "+port);
+
